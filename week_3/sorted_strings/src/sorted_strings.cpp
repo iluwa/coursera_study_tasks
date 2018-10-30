b522bab43f682e1bf7e4066a54047b6bd1c59a1f
ikolyaskin <noobikflash@yandex.ru>
2018-10-30 18:53:48 +0300

    add sorted_strings task

---
+++ week_3/sorted_strings/src/sorted_strings.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class SortedStrings {
+public:
+	void AddString(const string& s) {
+		v.push_back(s);
+	}
+
+	vector<string> GetSortedStrings() {
+		sort(v.begin(), v.end());
+		return v;
+	}
+private:
+	vector<string> v;
+};
+/*
+void PrintSortedStrings(SortedStrings& strings) {
+	for (const string& s : strings.GetSortedStrings()) {
+		cout << s << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	SortedStrings strings;
+
+	strings.AddString("first");
+	strings.AddString("third");
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+	return 0;
+}
+*/
