674868693253205358030f7168a5cb848bc10806
ikolyaskin <ikolyaskin@phoenixit.ru>
2018-11-02 14:32:34 +0300

    uppgrade person to person2 task

--- week_3/person/src/person.cpp
+++ week_3/person/src/person.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <set>
 using namespace std;
 
 /*struct FullName {
@@ -7,45 +8,86 @@ using namespace std;
 	string last_name;
 };*/
 
-int findNearestKey(const map<int, string>& m, int target) {
-	int nearest_key = 0;
-	for (auto rit = m.rbegin(); rit != m.rend(); ++rit) {
-		if (rit -> first <= target) {
-			nearest_key = rit -> first;
-			break;
-		}
-	}
-	return nearest_key;
-}
-
 class Person {
 public:
 	void ChangeFirstName(int year, const string& first_name) {
+		// добавить факт изменения имени на first_name в год year
 		first_name_map[year] = first_name;
 	}
+
 	void ChangeLastName(int year, const string& last_name) {
+		// добавить факт изменения фамилии на last_name в год year
 		last_name_map[year] = last_name;
 	}
+
 	string GetFullName(int year) {
-		int first_name_key = findNearestKey(first_name_map, year);
-		int last_name_key = findNearestKey(last_name_map, year);
-		int first_name_count = first_name_map.count(first_name_key);
-		int last_name_count = last_name_map.count(last_name_key);
-		if (first_name_count == 0 && last_name_count == 0) {
-			return "Incognito";
-		} else {
-			if (last_name_count == 0 && first_name_count > 0) {
-				return first_name_map[first_name_key] + " with unknown last name";
-			} else if (first_name_count == 0 && last_name_count > 0) {
-				return last_name_map[last_name_key] + " with unknown firsrt name";
-			} else {
-				return first_name_map[first_name_key] + " " + last_name_map[last_name_key];
-			}
-		}
+		// получить имя и фамилию по состоянию на конец года year
+		string first_name = GetNearestMapValue(first_name_map, year);
+		string last_name = GetNearestMapValue(last_name_map, year);
+		return FormProperOut(first_name, last_name);
+	}
+
+	string GetFullNameWithHistory(int year) {
+		// получить все имена и фамилии по состоянию на конец года year
+		string first_name = GetHistoryFromMapValues(first_name_map, year);
+		string last_name = GetHistoryFromMapValues(last_name_map, year);
+		return FormProperOut(first_name, last_name);
 	}
 private:
 	map<int, string> last_name_map;
 	map<int, string> first_name_map;
+
+	string GetNearestMapValue(const map<int, string>& m, int target) {
+		string nearest_value = "";
+		for (auto rit = m.rbegin(); rit != m.rend(); ++rit) {
+			if (rit -> first <= target) {
+				nearest_value = rit -> second;
+				break;
+			}
+		}
+		return nearest_value;
+	}
+
+	string GetHistoryFromMapValues(const map<int, string>& m, int target) {
+		string actual = "";
+		string history = "";
+		set<string> uniq_validation;
+		for (auto rit = m.rbegin(); rit != m.rend(); ++rit) {
+			if (rit -> first <= target) {
+				if (actual == "") {
+					actual = rit -> second;
+					uniq_validation.insert(rit -> second);
+				} else {
+					if (uniq_validation.count(rit -> second) == 0 && rit -> second != actual) {
+						if (history == "") {
+							history += " (" + rit -> second;
+						} else {
+							history += ", " + rit -> second;
+						}
+					}
+				}
+			}
+		}
+		if (history != "") {
+			history += ")";
+		}
+		return actual + history;
+	}
+
+	string FormProperOut(string first_name, string last_name) {
+		if (first_name == "" && last_name == "") {
+			return "Incognito";
+		} else {
+			if (last_name == "" && first_name != "") {
+				return first_name + " with unknown last name";
+			} else if (first_name == "" && last_name != "") {
+				return last_name + " with unknown firsrt name";
+			} else {
+				return first_name + " " + last_name;
+			}
+		}
+	}
+
 };
 
 int main() {
@@ -54,17 +96,32 @@ int main() {
 	person.ChangeFirstName(1965, "Polina");
 	person.ChangeLastName(1967, "Sergeeva");
 	for (int year : {1900, 1965, 1990}) {
-		cout << person.GetFullName(year) << endl;
+	cout << person.GetFullNameWithHistory(year) << endl;
 	}
 
 	person.ChangeFirstName(1970, "Appolinaria");
 	for (int year : {1969, 1970}) {
-		cout << person.GetFullName(year) << endl;
+	cout << person.GetFullNameWithHistory(year) << endl;
 	}
 
 	person.ChangeLastName(1968, "Volkova");
 	for (int year : {1969, 1970}) {
-		cout << person.GetFullName(year) << endl;
+	cout << person.GetFullNameWithHistory(year) << endl;
 	}
+
+	person.ChangeFirstName(1990, "Polina");
+	person.ChangeLastName(1990, "Volkova-Sergeeva");
+	cout << person.GetFullNameWithHistory(1990) << endl;
+
+	person.ChangeFirstName(1966, "Pauline");
+	cout << person.GetFullNameWithHistory(1966) << endl;
+
+	person.ChangeLastName(1960, "Sergeeva");
+	for (int year : {1960, 1967}) {
+	cout << person.GetFullNameWithHistory(year) << endl;
+	}
+
+	person.ChangeLastName(1961, "Ivanova");
+	cout << person.GetFullNameWithHistory(1967) << endl;
 	return 0;
 }
